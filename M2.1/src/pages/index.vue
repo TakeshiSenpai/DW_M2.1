e52528e45c91462d11dc4b2786648213e5739335
Alan <alan.11.gt@gmail.com>
2024-03-12 21:28:36 -0700

    M2.1Final

---
+++ M2.1/src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-content>
+      <Navbar />
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+</script>
---
+++ M2.1/src/pages/tabla.vue
@@ -0,0 +1,37 @@
+<template>
+    <Navbar />
+    <v-container>
+        <v-data-table :headers="headers" :items="todos" class="elevation-1"></v-data-table>
+    </v-container>
+</template>
+  
+<script>
+import axios from 'axios';
+
+export default {
+    data: () => ({
+        todos: [], 
+        headers: [ 
+            { title: 'ID', key: 'id' },
+            { title: 'Title', key: 'title' },
+            { title: 'Completed', key: 'completed' },
+        ],
+    }),
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+                this.todos = response.data;
+            } catch (error) {
+                console.error('There was an error fetching the data:', error);
+            }
+        }
+
+    },
+};
+
+</script>
+  
